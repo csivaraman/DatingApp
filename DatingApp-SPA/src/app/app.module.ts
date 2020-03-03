3f6317e325f532fad5c4562d7e8c94ad2444ae2f
csivaraman <csivaraman1808@gmail.com>
2020-03-02 20:20:52 -0500

    updated resources

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -22,6 +22,9 @@ import { MemberCardComponent } from './members/member-list/member-card/member-ca
 import { MemberDetailedComponent } from './members/member-list/member-detailed/member-detailed.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-list/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes-guard';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -37,7 +40,8 @@ export function tokenGetter() {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailedComponent
+      MemberDetailedComponent,
+      MemberEditComponent
    ],
    imports: [
       BrowserModule,
@@ -60,7 +64,9 @@ export function tokenGetter() {
       AuthService,
       ErrorInterceptorProvider,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
